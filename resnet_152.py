d2d042faad17211c85d41563cf47f24fe8e56a37
minchoe828-gif <minchoe828@gmail.com>
2025-10-11 14:26:22 +0900

    Update resnet_152.py

--- resnet_152.py
+++ resnet_152.py
@@ -78,3 +78,32 @@ class Res_152(nn.Module):
         X=self.resblk_4(X)
         X=self.lastblock(X)
         return X
+
+class ResBlock_v2(nn.Module):
+    def __init__(self, block_size, use_1x1conv,stride=1):
+        super().__init__()
+        self.out_channels=block_size[-1][-1]
+        if use_1x1conv:
+            self.conv1x1 = nn.LazyConv2d(self.out_channels,kernel_size=1, stride=stride)
+        else:
+            self.conv1x1=None
+        self.module_sequence = nn.Sequential()
+        for i, conv_size in enumerate(block_size):
+            kernel_size, out_channels = conv_size
+            padding_size = (kernel_size-1)//2
+            self.module_sequence.add_module(nn.LazyBatchNorm2d())
+            self.module_sequence.add_module(nn.ReLU())
+            if i==0:
+                self.module_sequence.add_module(nn.LazyConv2d(out_channels, kernel_size = kernel_size, padding=padding_size, stride=stride))
+            else:
+                self.module_sequence.add_module(nn.LazyConv2d(out_channels, kernel_size = kernel_size, padding=padding_size))
+    
+    
+    def forward(self,X):
+        if self.conv1x1:
+            identity = self.conv1x1(X)
+        else:
+            identity = X
+        Y=self.module_sequence(X)
+        Y+=identity 
+        return Y
