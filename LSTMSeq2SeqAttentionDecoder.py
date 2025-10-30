57cf1872d684bb122aad2053ef4f6da1d3b8ab7f
minchoe828-gif <minchoe828@gmail.com>
2025-10-30 19:00:51 +0900

    Update LSTMSeq2SeqAttentionDecoder.py

--- LSTMSeq2SeqAttentionDecoder.py
+++ LSTMSeq2SeqAttentionDecoder.py
@@ -6,4 +6,20 @@ class MyLSTMSeq2SeqAttentionDecoder(AttentionDecoder):
         self.attention = d2l.AdditiveAttention(num_hiddens, dropout=dropout)
         self.dense = nn.LazyLinear(vocab_size)
 
-    
+    def forward(self, X, state):
+        #X:(batch, sequence) -> (batch, sequence, embed_size)
+        X=self.embedding(X)
+        #X:(b,s,e) -> (s, b, e) 
+        X.permute(1,0,2)
+        #enc_output:(sequence, batch, hidden)
+        #hidden_state:(layer, batch, hidden)
+        enc_output, hidden_state, enc_valid_lens = state
+        for x in X:
+            #query:(batch, hidden) -> (batch, num of query=1, hidden)
+            query = hidden_state[-1].unsqueeze(1)
+            #context : (Batch, num of query=1, hidden)
+            context = self.attention(query, enc_output, enc_output, enc_valid_lens)
+            x=torch.cat((context, x.unsqueeze(1)), dim=-1)
+            
+        
+        
