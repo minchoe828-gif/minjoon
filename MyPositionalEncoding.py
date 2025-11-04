9ac1749b49ae1108589fa5835bd00df25d177100
minchoe828-gif <minchoe828@gmail.com>
2025-11-04 23:45:34 +0900

    Create MyPositionalEncoding.py

---
+++ MyPositionalEncoding.py
@@ -0,0 +1,15 @@
+class MyPositionalEncoding(nn.Module):
+    def __init__(self, max_sequences, num_embed):
+        super().__init__()
+        self.P = torch.zeros((max_sequences, num_embed))
+        X_row = torch.arange(max_sequences).unsqueeze(1)
+        X_column = torch.pow(10000, torch.arange(0,num_embed,2)/num_embed)
+        X = X_row/X_column
+        self.P[:, 0::2] = torch.sin(X)
+        self.P[:, 1::2] = torch.cos(X)
+
+    def forward(self, X):
+        X += self.P[:X.shape[1],:].to(X.device).unsqueeze(0)
+        return X
+        
+        
