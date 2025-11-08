e8ef72fe9c160713434209b2c0719e0d0d469936
minchoe828-gif <minchoe828@gmail.com>
2025-11-08 13:16:26 +0900

    Create AddNorm.py

---
+++ AddNorm.py
@@ -0,0 +1,8 @@
+class AddNorm(nn.Module):
+    def __init__(self, norm_shape, dropout):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.ln = nn.LayerNorm(norm_shape)
+
+    def forward(self, X, Y):
+        return self.ln(self.dropout(Y)+X)
