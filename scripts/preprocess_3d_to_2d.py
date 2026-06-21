7aef60e526b55fb2d2303c8eac381d72ea86ad79
minchoe828-gif <minchoe828@gmail.com>
2026-06-21 14:24:44 +0900

    Add type hints to build_dataset_and_metadata function
    Updated the function signature of build_dataset_and_metadata to include type hints for parameters.

--- scripts/preprocess_3d_to_2d.py
+++ scripts/preprocess_3d_to_2d.py
@@ -4,7 +4,12 @@ from aicsimageio import AICSImage
 from pathlib import Path
 from tqdm import tqdm
 
-def build_dataset_and_metadata(raw_dir, processed_dir, input_ch, target_ch, min_pixels=100):
+#다운받은 cell 이미지들을 2d numpy로 변환하여 dataset을 생성할 때 더 빠르게 데이터를 가져올 수 있도록 한다.
+def build_dataset_and_metadata(raw_dir: str | Path,
+                               processed_dir: str | Path,
+                               input_ch: int,
+                               target_ch: int
+                              ):
     raw_dir = Path(raw_dir)
     processed_dir = Path(processed_dir)
     processed_dir.mkdir(parents=True, exist_ok=True)
@@ -13,8 +18,8 @@ def build_dataset_and_metadata(raw_dir, processed_dir, input_ch, target_ch, min_
 
     for tif_path in tqdm(list(raw_dir.glob('*.tif*'))):
         reader = AICSImage(tif_path)
-        target_3d = reader.get_image_data('ZYX', T=0, C=target_ch)
         input_3d = reader.get_image_data('ZYX', T=0, C=input_ch)
+        target_3d = reader.get_image_data('ZYX', T=0, C=target_ch)
 
         for z_idx in range(target_3d.shape[0]):
             if np.sum(target_3d[z_idx] >0) >= min_pixels:
